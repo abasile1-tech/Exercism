9d663603c00e13257f0a41034fca411ba1a7e5d5
John <ashton.basile@gmail.com>
2023-07-11 15:01:40 +0100

    I solved the value_of_card function

--- python/black-jack/black_jack.py
+++ python/black-jack/black_jack.py
@@ -15,8 +15,12 @@ def value_of_card(card):
     2.  'A' (ace card) = 1
     3.  '2' - '10' = numerical value.
     """
-
-    pass
+    if card == 'J' or card == 'Q' or card == 'K':
+        return 10
+    elif card == 'A':
+        return 1
+    else:
+        return int(card)
 
 
 def higher_card(card_one, card_two):
