53395666a4f94693c7ce7d7d0e42dd1b6fabac7d
John <ashton.basile@gmail.com>
2024-01-31 16:42:00 +0000

    All of the tests are passing locally

--- csharp/annalyns-infiltration/AnnalynsInfiltration.cs
+++ csharp/annalyns-infiltration/AnnalynsInfiltration.cs
@@ -1,24 +1,38 @@
-using System;
-
 static class QuestLogic
 {
     public static bool CanFastAttack(bool knightIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFastAttack() method");
+        if (!knightIsAwake)
+        {
+            return true;
+        }
+        return false;
     }
 
     public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSpy() method");
+        if (knightIsAwake | archerIsAwake | prisonerIsAwake)
+        {
+            return true;
+        }
+        return false;
     }
 
     public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSignalPrisoner() method");
+        if (prisonerIsAwake && !archerIsAwake)
+        {
+            return true;
+        }
+        return false;
     }
 
     public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFreePrisoner() method");
+        if ((petDogIsPresent && !archerIsAwake) || (!petDogIsPresent && prisonerIsAwake && !knightIsAwake && !archerIsAwake))
+        {
+            return true;
+        }
+        return false;
     }
 }
--- csharp/annalyns-infiltration/Program.cs
+++ csharp/annalyns-infiltration/Program.cs
@@ -2,3 +2,7 @@ using System;
 
 Console.WriteLine("Hello World!");
 QuestLogic.CanFastAttack(false);
+var testObject = new AnnalynsInfiltrationTests();
+testObject.Cannot_execute_fast_attack_if_knight_is_awake();
+
+// to run tests, open up the Test Explorer by clicking on Test and then on Test Explorer.
