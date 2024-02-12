94d64a855b7135cfa3a0b07c94ba48c0fbf0c28d
John <ashton.basile@gmail.com>
2024-02-12 14:17:30 +0000

    All of the tests passed

--- csharp/elons-toys/ElonsToys.cs
+++ csharp/elons-toys/ElonsToys.cs
@@ -1,24 +1,34 @@
-using System;
-
 class RemoteControlCar
 {
+    private int distanceDriven;
+    private int batteryPercentage;
+
+    public RemoteControlCar()
+    {
+        distanceDriven = 0; batteryPercentage = 100;
+    }
     public static RemoteControlCar Buy()
     {
-        throw new NotImplementedException("Please implement the (static) RemoteControlCar.Buy() method");
+        return new RemoteControlCar();
     }
 
     public string DistanceDisplay()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDisplay() method");
+        return $"Driven {distanceDriven} meters";
     }
 
     public string BatteryDisplay()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDisplay() method");
+        if (batteryPercentage == 0) return $"Battery empty";
+        return $"Battery at {batteryPercentage}%";
     }
 
     public void Drive()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+        if (batteryPercentage == 0) return;
+        distanceDriven += 20;
+        batteryPercentage -= 1;
+
+
     }
 }
